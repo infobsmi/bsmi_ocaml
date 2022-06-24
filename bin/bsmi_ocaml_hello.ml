4544405dc0adc6f6293af185b4d3663899661e30
cnmade <hufeng1987@gmail.com>
2022-06-24 17:36:57 +0800

    错误的示范

--- bin/bsmi_ocaml_hello.ml
+++ bin/bsmi_ocaml_hello.ml
@@ -1,2 +1,4 @@
 
+let  rec astr = Bsmi_ocaml.Netrc.readAsString ()
+print_endline astr
 print_endline "Hello, world!"
--- lib/netrc.ml
+++ lib/netrc.ml
@@ -1,5 +1,5 @@
 module Netrc = struct
-  let readAsString() = 
+  let  readAsString(): string = 
      let ch = open_in "~/.netrc" in
      let s = really_input_string ch (in_channel_length ch) in
      close_in ch;
