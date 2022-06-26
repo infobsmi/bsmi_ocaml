f51aef4bb3559d97424c14e31b40e8db6f329b56
cnmade <hufeng1987@gmail.com>
2022-06-26 12:57:27 +0800

    调用函数方法

--- bin/bsmi_ocaml_hello.ml
+++ bin/bsmi_ocaml_hello.ml
@@ -1,8 +1,10 @@
 open Bsmi_ocaml.Netrc
+let i = "a world";;
 
 let hello () = print_endline "Gear box, i am from bsmi_ocaml_hello.ml"
 let () =
   hello ();
-  Netrc.hello ();
+  Netrc.hello (); 
+  print_endline i;
   let a  = Netrc.readAsString () in
-  print_endline a
+  print_endline a;
