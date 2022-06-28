3fb4c862fa15a5f741bcc00e965b64158026a69f
cnmade <hufeng1987@gmail.com>
2022-06-28 11:21:22 +0800

    更新正则处理字符串的逻辑

--- bin/bsmi_ocaml_hello.ml
+++ bin/bsmi_ocaml_hello.ml
@@ -1,10 +1,21 @@
 open Bsmi_ocaml.Netrc
+
+
+let netrcStr: string = "machine google.com  login admin password hellomotog"
+let re_whitespace = Re.Posix.compile_pat "[\t ]+"
+
+
 let i = "a world";;
 
+
 let hello () = print_endline "Gear box, i am from bsmi_ocaml_hello.ml"
+
 let () =
   hello ();
   Netrc.hello (); 
   print_endline i;
   let a  = Netrc.readAsString () in
   print_endline a;
+  let strList = Re.split re_whitespace netrcStr in 
+  let twoStr =  String.concat "," strList in
+  print_endline twoStr;;
