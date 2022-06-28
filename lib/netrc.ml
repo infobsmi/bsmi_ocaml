afafe65ba6b1fc8d9184bf147935f11424977776
cnmade <hufeng1987@gmail.com>
2022-06-28 16:30:56 +0800

    更新mli文件

--- lib/netrc.ml
+++ lib/netrc.ml
@@ -1,10 +1,12 @@
 
-module type Netrc_type = sig
-  val readAsString: unit -> string
-  val readHelloWorld: unit -> string
-  val hello: unit -> unit
+module type Netrc_type = 
+sig
+  val readAsString : unit -> string
+  val readHelloWorld : unit -> string
+  val hello : unit -> unit
 end
-module Netrc: Netrc_type = struct
+
+module Netrc : Netrc_type = struct
   let  readAsString ()  = 
      let ch = open_in "/etc/resolv.conf" in
      let s = really_input_string ch (in_channel_length ch) in
