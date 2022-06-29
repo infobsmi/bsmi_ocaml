f76f949afcba0ad1178b9233864d27b7ca3c38b9
cnmade <hufeng1987@gmail.com>
2022-06-29 17:12:47 +0800

    Update format document

--- lib/timeutils.mli
+++ lib/timeutils.mli
@@ -1,13 +1,15 @@
 module Timeutils : sig
-  val now : unit -> float
+
   (** get now time return float number *)
+    val now : unit -> float
 
-  val nowAsInt : unit -> int
   (** get now time return   seconds of unix timestamp *)
+  val nowAsInt : unit -> int
 
-  val nowAsMilsInt : unit -> int
   (** get now time return mils seconds of unix timestamp *)
 
-  val nowAsMicroInt : unit -> int
+  val nowAsMilsInt : unit -> int
   (** get now time return micro seconds of unix timestamp *)
+
+  val nowAsMicroInt : unit -> int
 end
