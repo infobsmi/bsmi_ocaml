3e44cbf8448d4fe5784f425a41dc4b3bccde83ee
cnmade <hufeng1987@gmail.com>
2022-06-29 11:17:42 +0800

    Update bsmi Timeutils

--- bin/bsmi_ocaml_hello.ml
+++ bin/bsmi_ocaml_hello.ml
@@ -20,4 +20,7 @@ let () =
   let strList = Re.split re_whitespace netrcStr in 
   let twoStr =  String.concat "," strList in
   print_endline twoStr;;
-  print_endline (string_of_float (Timeutils.now () ));;
+  Printf.printf "%.6f\n" (Timeutils.now () );;
+  print_endline (string_of_int (Timeutils.nowAsInt ()));;
+  print_endline (string_of_int (Timeutils.nowAsMilsInt ()));;
+  print_endline (string_of_int (Timeutils.nowAsMicroInt ()));;
--- lib/timeutils.mli
+++ lib/timeutils.mli
@@ -1,1 +1,15 @@
-module Timeutils : sig val now : unit -> float end
+module Timeutils :
+  sig
+
+  (** get now time return float number *)
+    val now : unit -> float
+
+  (** get now time return   seconds of unix timestamp *)
+    val nowAsInt : unit -> int
+
+  (** get now time return mils seconds of unix timestamp *)
+    val nowAsMilsInt : unit -> int
+    
+  (** get now time return micro seconds of unix timestamp *)
+    val nowAsMicroInt : unit -> int
+  end
