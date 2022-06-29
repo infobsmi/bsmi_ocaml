b5bc45f4184215d30d1f2a144380c3df4a046251
cnmade <hufeng1987@gmail.com>
2022-06-29 21:17:00 +0800

    Recreate desc

--- lib/phoneutils.ml
+++ lib/phoneutils.ml
@@ -1,7 +1,4 @@
 module Phoneutils = struct
-  (**  验证输入的号码是否有效：移动号码段:139、138、137、136、135、134、150、151、152、157、158、159、182、183、187、188、147
-  联通号码段:130、131、132、136、185、186、145
-  电信号码段:133、153、180、189 *)
   let isValidCnPhone phone : bool =
     if String.length phone <> 11 then false
     else
